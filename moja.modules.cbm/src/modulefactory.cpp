23c1255db396a0fecbe43752a6373746ce4cd0d9
malfrancis <malcolm.francis@bowrush.com>
2015-11-03 13:38:36 +1100

    more linux

--- moja.modules.cbm/src/modulefactory.cpp
+++ moja.modules.cbm/src/modulefactory.cpp
@@ -16,89 +16,78 @@
 #include "moja/modules/cbm/record.h"
 #include "moja/modules/cbm/recordaccumulator.h"
 
-using moja::flint::IModule;
-using moja::flint::ITransform;
-using moja::flint::ModuleRegistration;
-using moja::flint::TransformRegistration;
-
-using moja::modules::cbm::RecordAccumulator;
-using moja::modules::cbm::DateRow;
-using moja::modules::cbm::PoolInfoRow;
-using moja::modules::cbm::ClassifierSetRow;
-using moja::modules::cbm::LocationRow;
-
 namespace moja {
 namespace modules {
 
     struct CBMObjectHolder {
         CBMObjectHolder() {
-            dateDimension = std::make_shared<RecordAccumulator<DateRow>>();
-            poolInfoDimension = std::make_shared<RecordAccumulator<PoolInfoRow>>();
-            classifierSetDimension = std::make_shared<RecordAccumulator<ClassifierSetRow>>();
-            locationDimension = std::make_shared<RecordAccumulator<LocationRow>>();
+            dateDimension = std::make_shared<cbm::RecordAccumulator<cbm::DateRow>>();
+            poolInfoDimension = std::make_shared<cbm::RecordAccumulator<cbm::PoolInfoRow>>();
+            classifierSetDimension = std::make_shared<cbm::RecordAccumulator<cbm::ClassifierSetRow>>();
+            locationDimension = std::make_shared<cbm::RecordAccumulator<cbm::LocationRow>>();
         }
 
-        std::shared_ptr<RecordAccumulator<DateRow>> dateDimension;
-        std::shared_ptr<RecordAccumulator<PoolInfoRow>> poolInfoDimension;
-        std::shared_ptr<RecordAccumulator<ClassifierSetRow>> classifierSetDimension;
-        std::shared_ptr<RecordAccumulator<LocationRow>> locationDimension;
+        std::shared_ptr<cbm::RecordAccumulator<cbm::DateRow>> dateDimension;
+        std::shared_ptr<cbm::RecordAccumulator<cbm::PoolInfoRow>> poolInfoDimension;
+        std::shared_ptr<cbm::RecordAccumulator<cbm::ClassifierSetRow>> classifierSetDimension;
+        std::shared_ptr<cbm::RecordAccumulator<cbm::LocationRow>> locationDimension;
     };
 
     CBMObjectHolder cbmObjectHolder;
 
     extern "C" {
 
-        MOJA_LIB_API moja::flint::IModule* CreateCBMAggregatorPoolSQLite() {
-            return new moja::modules::cbm::CBMAggregatorPoolSQLite(
+        MOJA_LIB_API flint::IModule* CreateCBMAggregatorPoolSQLite() {
+            return new cbm::CBMAggregatorPoolSQLite(
                 cbmObjectHolder.dateDimension,
                 cbmObjectHolder.poolInfoDimension,
                 cbmObjectHolder.classifierSetDimension,
                 cbmObjectHolder.locationDimension);
         }
 
-        MOJA_LIB_API moja::flint::IModule* CreateCBMAggregatorFluxSQLite() {
-            return new moja::modules::cbm::CBMAggregatorFluxSQLite(
+        MOJA_LIB_API flint::IModule* CreateCBMAggregatorFluxSQLite() {
+            return new cbm::CBMAggregatorFluxSQLite(
                 cbmObjectHolder.dateDimension,
                 cbmObjectHolder.poolInfoDimension,
                 cbmObjectHolder.classifierSetDimension,
                 cbmObjectHolder.locationDimension);
         }
 
-        MOJA_LIB_API moja::flint::IModule* CreateCBMDecayModule					() { return new moja::modules::cbm::CBMDecayModule				();	}
-        MOJA_LIB_API moja::flint::IModule* CreateCBMDisturbanceEventModule		() { return new moja::modules::cbm::CBMDisturbanceEventModule	(); }
-        MOJA_LIB_API moja::flint::IModule* CreateCBMGrowthModule				() { return new moja::modules::cbm::YieldTableGrowthModule		();	}
-        MOJA_LIB_API moja::flint::IModule* CreateCBMTurnoverModule				() { return new moja::modules::cbm::CBMTurnoverModule			();	}
-        MOJA_LIB_API moja::flint::IModule* CreateCBMSequencer					() { return new moja::modules::cbm::CBMSequencer				();	}
-        MOJA_LIB_API moja::flint::IModule* CreateOutputerStreamPostNotify		() { return new moja::modules::cbm::OutputerStreamPostNotify	(); }
-        MOJA_LIB_API moja::flint::IModule* CreateOutputerStreamFluxPostNotify	() { return new moja::modules::cbm::OutputerStreamFluxPostNotify(); }
-        MOJA_LIB_API moja::flint::IModule* CreateCBMSpinupSequencer				() { return new moja::modules::cbm::CBMSpinupSequencer			();	}
-        MOJA_LIB_API moja::flint::IModule* CreateCBMBuildLandUnitModule			() { return new moja::modules::cbm::CBMBuildLandUnitModule		(); }
-        MOJA_LIB_API moja::flint::IModule* CreateCBMSpinupDisturbanceModule		() { return new moja::modules::cbm::CBMSpinupDisturbanceModule  (); }
+        MOJA_LIB_API flint::IModule* CreateCBMDecayModule					() { return new cbm::CBMDecayModule				 (); }
+        MOJA_LIB_API flint::IModule* CreateCBMDisturbanceEventModule		() { return new cbm::CBMDisturbanceEventModule	 (); }
+        MOJA_LIB_API flint::IModule* CreateCBMGrowthModule					() { return new cbm::YieldTableGrowthModule		 (); }
+        MOJA_LIB_API flint::IModule* CreateCBMTurnoverModule				() { return new cbm::CBMTurnoverModule			 (); }
+        MOJA_LIB_API flint::IModule* CreateCBMSequencer						() { return new cbm::CBMSequencer				 (); }
+        MOJA_LIB_API flint::IModule* CreateOutputerStreamPostNotify			() { return new cbm::OutputerStreamPostNotify	 (); }
+        MOJA_LIB_API flint::IModule* CreateOutputerStreamFluxPostNotify		() { return new cbm::OutputerStreamFluxPostNotify(); }
+        MOJA_LIB_API flint::IModule* CreateCBMSpinupSequencer				() { return new cbm::CBMSpinupSequencer			 (); }
+        MOJA_LIB_API flint::IModule* CreateCBMBuildLandUnitModule			() { return new cbm::CBMBuildLandUnitModule		 (); }
+        MOJA_LIB_API flint::IModule* CreateCBMSpinupDisturbanceModule		() { return new cbm::CBMSpinupDisturbanceModule  (); }
 
-        MOJA_LIB_API ITransform* CreateCBMLandUnitDataTransform					() { return new moja::modules::cbm::CBMLandUnitDataTransform	(); }
-        MOJA_LIB_API ITransform* CreateGrowthCurveTransform						() { return new moja::modules::cbm::GrowthCurveTransform		(); }
+        MOJA_LIB_API flint::ITransform* CreateCBMLandUnitDataTransform		() { return new cbm::CBMLandUnitDataTransform	 (); }
+        MOJA_LIB_API flint::ITransform* CreateGrowthCurveTransform			() { return new cbm::GrowthCurveTransform		 (); }
 
         MOJA_LIB_API int getModuleRegistrations(moja::flint::ModuleRegistration* outModuleRegistrations) {
             int index = 0;
-            outModuleRegistrations[index++] = moja::flint::ModuleRegistration{ "CBMAggregatorPoolSQLite",		&CreateCBMAggregatorPoolSQLite };
-            outModuleRegistrations[index++] = moja::flint::ModuleRegistration{ "CBMAggregatorFluxSQLite",       &CreateCBMAggregatorFluxSQLite };
-            outModuleRegistrations[index++] = moja::flint::ModuleRegistration{ "CBMDecayModule",                &CreateCBMDecayModule };
-            outModuleRegistrations[index++] = moja::flint::ModuleRegistration{ "CBMDisturbanceEventModule",	    &CreateCBMDisturbanceEventModule };
-            outModuleRegistrations[index++] = moja::flint::ModuleRegistration{ "CBMGrowthModule",               &CreateCBMGrowthModule };
-            outModuleRegistrations[index++] = moja::flint::ModuleRegistration{ "CBMTurnoverModule",             &CreateCBMTurnoverModule };
-            outModuleRegistrations[index++] = moja::flint::ModuleRegistration{ "CBMSequencer",				    &CreateCBMSequencer };
-            outModuleRegistrations[index++] = moja::flint::ModuleRegistration{ "OutputerStreamPostNotify",	    &CreateOutputerStreamPostNotify };
-            outModuleRegistrations[index++] = moja::flint::ModuleRegistration{ "OutputerStreamFluxPostNotify",  &CreateOutputerStreamFluxPostNotify };
-            outModuleRegistrations[index++] = moja::flint::ModuleRegistration{ "CBMSpinupSequencer",			&CreateCBMSpinupSequencer };
-            outModuleRegistrations[index++] = moja::flint::ModuleRegistration{ "CBMBuildLandUnitModule",		&CreateCBMBuildLandUnitModule };
-            outModuleRegistrations[index++] = moja::flint::ModuleRegistration{ "CBMSpinupDisturbanceModule",    &CreateCBMSpinupDisturbanceModule };
+            outModuleRegistrations[index++] = flint::ModuleRegistration{ "CBMAggregatorPoolSQLite",		 &CreateCBMAggregatorPoolSQLite };
+            outModuleRegistrations[index++] = flint::ModuleRegistration{ "CBMAggregatorFluxSQLite",      &CreateCBMAggregatorFluxSQLite };
+            outModuleRegistrations[index++] = flint::ModuleRegistration{ "CBMDecayModule",               &CreateCBMDecayModule };
+            outModuleRegistrations[index++] = flint::ModuleRegistration{ "CBMDisturbanceEventModule",	 &CreateCBMDisturbanceEventModule };
+            outModuleRegistrations[index++] = flint::ModuleRegistration{ "CBMGrowthModule",              &CreateCBMGrowthModule };
+            outModuleRegistrations[index++] = flint::ModuleRegistration{ "CBMTurnoverModule",            &CreateCBMTurnoverModule };
+            outModuleRegistrations[index++] = flint::ModuleRegistration{ "CBMSequencer",				 &CreateCBMSequencer };
+            outModuleRegistrations[index++] = flint::ModuleRegistration{ "OutputerStreamPostNotify",	 &CreateOutputerStreamPostNotify };
+            outModuleRegistrations[index++] = flint::ModuleRegistration{ "OutputerStreamFluxPostNotify", &CreateOutputerStreamFluxPostNotify };
+            outModuleRegistrations[index++] = flint::ModuleRegistration{ "CBMSpinupSequencer",			 &CreateCBMSpinupSequencer };
+            outModuleRegistrations[index++] = flint::ModuleRegistration{ "CBMBuildLandUnitModule",		 &CreateCBMBuildLandUnitModule };
+            outModuleRegistrations[index++] = flint::ModuleRegistration{ "CBMSpinupDisturbanceModule",   &CreateCBMSpinupDisturbanceModule };
             return index;
         }
 
-        MOJA_LIB_API int getTransformRegistrations(moja::flint::TransformRegistration* outTransformRegistrations) {
+        MOJA_LIB_API int getTransformRegistrations(flint::TransformRegistration* outTransformRegistrations) {
             int index = 0;
-            outTransformRegistrations[index++] = TransformRegistration{ "CBMLandUnitDataTransform", &CreateCBMLandUnitDataTransform };
-            outTransformRegistrations[index++] = TransformRegistration{ "GrowthCurveTransform",     &CreateGrowthCurveTransform };
+            outTransformRegistrations[index++] = flint::TransformRegistration{ "CBMLandUnitDataTransform", &CreateCBMLandUnitDataTransform };
+            outTransformRegistrations[index++] = flint::TransformRegistration{ "GrowthCurveTransform",     &CreateGrowthCurveTransform };
             return index;
         }
 
