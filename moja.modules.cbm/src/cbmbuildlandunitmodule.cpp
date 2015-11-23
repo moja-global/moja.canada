441e6a73c19cd2e8ff3463f0d4d54ad97cf2ce11
mfellows <mfellows@nrcan.gc.ca>
2015-11-23 11:50:43 -0800

    CBM simulations now support deforestation events which transition stands to non-forest (no growth curve).

--- moja.modules.cbm/src/cbmbuildlandunitmodule.cpp
+++ moja.modules.cbm/src/cbmbuildlandunitmodule.cpp
@@ -18,9 +18,13 @@ namespace cbm {
     void CBMBuildLandUnitModule::onLocalDomainInit(const flint::LocalDomainInitNotification::Ptr& /*n*/) {
         _initialAge = _landUnitData->getVariable("initial_age");
         _buildWorked = _landUnitData->getVariable("landUnitBuildSuccess");
+        _initialGCID = _landUnitData->getVariable("initial_growth_curve_id");
+        _gcid = _landUnitData->getVariable("growth_curve_id");
     }
 
     void CBMBuildLandUnitModule::onPreTimingSequence(const flint::PreTimingSequenceNotification::Ptr& /*n*/) {
+        auto gcid = _initialGCID->value();
+        _gcid->set_value(gcid.isEmpty() ? -1 : gcid);
         bool success = !_initialAge->value().isEmpty();
         // What CBM variables need to be set between land unit simulations?
         // External variables are handled already, just internal variables. State values? Classifiers?
