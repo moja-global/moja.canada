b66f4ad53d31deb2367156687c202579b8cd6f7e
mfellows <mfellows@nrcan.gc.ca>
2016-09-09 14:17:44 -0700

    Added CBM3's 10/20-year land class remaining flip to land class transition module.

--- moja.modules.cbm/src/cbmdecaymodule.cpp
+++ moja.modules.cbm/src/cbmdecaymodule.cpp
@@ -80,11 +80,10 @@ namespace cbm {
     }
 
     void CBMDecayModule::onTimingInit() {
-
 		auto mat = _landUnitData->getVariable("mean_annual_temperature")->value();
 		_T = mat.isEmpty() ? 0
 			: mat.isTimeSeries() ? mat.extract<TimeSeries>().value()
-			: mat.extract<float>();
+			: mat.convert<double>();
 
 		_slowMixingRate = _landUnitData->getVariable("slow_ag_to_bg_mixing_rate")->value();
 
