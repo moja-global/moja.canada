630ee33e935907eb2c993ab4046655d4bb05e2e1
gazhang <gazhang@nrcan.gc.ca>
2017-07-25 11:35:16 -0700

    added moja search directory: "C:/Development/moja.global/dist"

--- Source/cmake/FindMoja.cmake
+++ Source/cmake/FindMoja.cmake
@@ -25,6 +25,7 @@ if(CMAKE_SYSTEM MATCHES "Windows")
 	if(EXISTS "C:/")
 		SET(MOJA_DIR_SEARCH
 			${MOJA_DIR_SEARCH}
+			"C:/Development/moja.global/dist"
 			"C:/Development/Software/moja"
 			"C:/moja"
 			"C:/Development/moja"
@@ -32,6 +33,7 @@ if(CMAKE_SYSTEM MATCHES "Windows")
 
 		SET(MOJA_LIB_DIR_SEARCH
 			${MOJA_LIB_DIR_SEARCH}
+			"C:/Development/moja.global/dist"
 			"C:/Development/Software/moja/"
 			"C:/moja/"
 			"C:/Development/moja/"
