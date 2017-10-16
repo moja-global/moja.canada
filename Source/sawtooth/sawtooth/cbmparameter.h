c268099cfef0a87b15d8ac398dd7b26a4b1e5ef5
Scott Morken <smorken@nrcan.gc.ca>
2017-10-16 12:00:40 -0700

    CBM extension will require disturbance matrices to compute losses when running in CBM mode

--- Source/sawtooth/sawtooth/cbmparameter.h
+++ Source/sawtooth/sawtooth/cbmparameter.h
@@ -54,7 +54,7 @@ namespace CBM {
 		double FineRootTurnProp;
 		TurnoverParameter() {}
 		TurnoverParameter(Cursor& c) {
-			id = c.GetValueInt32("id");;
+			id = c.GetValueInt32("id");
 			SoftwoodFoliageFallRate = c.GetValueDouble("SoftwoodFoliageFallRate");
 			HardwoodFoliageFallRate = c.GetValueDouble("HardwoodFoliageFallRate");
 			StemAnnualTurnoverRate = c.GetValueDouble("StemAnnualTurnoverRate");
@@ -65,5 +65,28 @@ namespace CBM {
 		}
 	};
 
+	struct DisturbanceMatrixAssociation {
+		int spatial_unit_id;
+		int disturbance_type;
+		int disturbance_matrix_id;
+		DisturbanceMatrixAssociation() {}
+		DisturbanceMatrixAssociation(Cursor& c) {
+			spatial_unit_id = c.GetValueInt32("spatial_unit_id");
+			disturbance_type = c.GetValueInt32("disturbance_type");
+			disturbance_matrix_id = c.GetValueInt32("disturbance_matrix_id");
+		}
+	};
+
+	struct DisturbanceMatrixValue {
+		int source_pool_id;
+		int sink_pool_id;
+		double value;
+		DisturbanceMatrixValue() {}
+		DisturbanceMatrixValue(Cursor& c) {
+			source_pool_id = c.GetValueInt32("source_pool_id");
+			sink_pool_id = c.GetValueInt32("sink_pool_id");
+			value = c.GetValueDouble("value");
+		}
+	};
 }}}
 #endif
