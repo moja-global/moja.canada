a53fdbdbfa06839cec8e92431e34f31a69993e95
mfellows <mfellows@nrcan.gc.ca>
2020-10-15 14:52:15 -0700

    Linux build fix.

--- Source/moja.modules.cbm/src/peatlandspinupnext.cpp
+++ Source/moja.modules.cbm/src/peatlandspinupnext.cpp
@@ -83,9 +83,8 @@ namespace cbm {
 
 		//get the mean anual temperture variable
 		auto meanAT = _landUnitData->getVariable("mean_annual_temperature")->value();	
-		auto defaultMAT = _landUnitData->getVariable("default_mean_annual_temperature")->value();
+		double defaultMAT = _landUnitData->getVariable("default_mean_annual_temperature")->value();
 		meanAnnualTemperature = meanAT.isEmpty() ? defaultMAT : meanAT.convert<double>();
-		
 
 		//get fire return interval
 		auto fireReturnInterval = _landUnitData->getVariable("fire_return_interval")->value();
