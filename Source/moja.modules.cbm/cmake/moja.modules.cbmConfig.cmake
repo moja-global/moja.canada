311bf0553023e2db0b08be5340dbbf0941442bb5
Max Fellows <max.fellows@canada.ca>
2021-01-28 15:02:23 -0800

    Fixed CMake files - should now be able to discover moja.modules.cbm from other projects using find_package:
    find_package(CBM REQUIRED moja.modules.cbm)
    target_link_libraries(${LIBNAME} CBM::moja.modules.cbm)

--- Source/moja.modules.cbm/cmake/moja.modules.cbmConfig.cmake
+++ Source/moja.modules.cbm/cmake/moja.modules.cbmConfig.cmake
@@ -3,6 +3,6 @@ find_dependency(moja REQUIRED COMPONENTS moja.flint)
 find_dependency(Poco REQUIRED COMPONENTS Foundation Data DataSQLite DataODBC JSON)
 find_dependency(PQXX REQUIRED)
 
-if(NOT TARGET moja::moja.modules.cbm)
-    include("${MojaModulesCBM_CMAKE_DIR}/moja.modules.cbmTargets.cmake")
+if(NOT TARGET CBM::moja.modules.cbm)
+    include("${CMAKE_CURRENT_LIST_DIR}/moja.modules.cbmTargets.cmake")
 endif()
