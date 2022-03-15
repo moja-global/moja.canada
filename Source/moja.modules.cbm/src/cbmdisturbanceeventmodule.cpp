bffb196222e118e6797afa2bedab02dbe29dd330
Max Fellows <max.fellows@canada.ca>
2022-03-15 15:43:08 -0700

    Fixed peatland guard in doLocalDomainInit - variable value starts off as false until set by a module, need to check if peatland modules as a whole are enabled in order to store the right pool and variable references; for that, use the "enable_peatland" variable.

--- Source/moja.modules.cbm/src/cbmdisturbanceeventmodule.cpp
+++ Source/moja.modules.cbm/src/cbmdisturbanceeventmodule.cpp
@@ -34,8 +34,8 @@ namespace moja {
 
 				_age = _landUnitData->getVariable("age");
 
-				if (_landUnitData->hasVariable("run_peatland") &&
-					_landUnitData->getVariable("run_peatland")->value()) {
+				if (_landUnitData->hasVariable("enable_peatland") &&
+					_landUnitData->getVariable("enable_peatland")->value()) {
 
 					_woodyFoliageLive = _landUnitData->getPool("WoodyFoliageLive");
 					_woodyStemsBranchesLive = _landUnitData->getPool("WoodyStemsBranchesLive");
@@ -59,7 +59,7 @@ namespace moja {
 				DynamicVar metadata = DynamicObject({
 					{ "disturbance", disturbanceType },
 					{ "disturbance_type_code", disturbanceCode }
-					});
+				});
 
 				auto disturbanceEvent = _landUnitData->createProportionalOperation(metadata);
 				auto transferVec = data["transfers"].extract<std::shared_ptr<std::vector<CBMDistEventTransfer>>>();
