906621e1d44389af6fbddb5547710b67bba24ee7
Aderibigbe Adeola <40776751+Adexandria@users.noreply.github.com>
2022-04-30 09:34:23 +0100

    Fixed spacing issues
    line 110 -112

--- Source/moja.modules.cbm/src/cbmaggregatorlibpqxxwriter.cpp
+++ Source/moja.modules.cbm/src/cbmaggregatorlibpqxxwriter.cpp
@@ -107,9 +107,9 @@ namespace cbm {
     * This is a function that loads the completed jobs,
     * pool dimension and classifer set dimension.
     * It also creates tables for the date dimension, land class dimension,
-	* module info dimension, location dimension, disturbance type dimension, 
-	* disturbance dimension,pools,fluxes,error dimension, age class dimension
-	* location error dimension, and age area if they do not already exist, and loads data into these tables.
+    * module info dimension, location dimension, disturbance type dimension,Â 
+    * disturbance dimension,pools,fluxes,error dimension, age class dimension
+    * location error dimension, and age area if they do not already exist, and loads data into these tables.
     * 
     *
     * @return void
