4baa99f3cfac46b55a49c11f7d1e4ced0f7212af
Namya LG <namyalg@gmail.com>
2022-06-04 12:11:06 +0530

    docs for componentbiomasscarboncurve

--- Source/moja.modules.cbm/src/componentbiomasscarboncurve.cpp
+++ Source/moja.modules.cbm/src/componentbiomasscarboncurve.cpp
@@ -4,11 +4,16 @@ namespace moja {
 namespace modules {
 namespace cbm {
 
-	/*
-	* Create a component biomass carbon curve 
-	* set the maximum age
-	* Request the vector size (maximum age + 1)
-	*/
+	/**
+	 * @brief Constructor
+	 * 
+	 * Assign ComponentBiomassCarbonCurve._maxAge as parameter maxAge , \n
+	 * The sizes of vectors ComponentBiomassCarbonCurve._merchCarbonIncrements, \n
+	 * ComponentBiomassCarbonCurve._foliageCarbonIncrements and ComponentBiomassCarbonCurve._otherCarbonIncrements \n
+	 * as maxAge + 1
+	 * 
+	 * @param maxAge int
+	 * **********************/
 	ComponentBiomassCarbonCurve::ComponentBiomassCarbonCurve(int maxAge)
 		: _maxAge(maxAge), _merchCarbonIncrements(maxAge + 1),
 		  _foliageCarbonIncrements(maxAge + 1), _otherCarbonIncrements(maxAge + 1) {}	
