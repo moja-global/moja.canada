a5ea79f7c30e477cb6299571fc1de046a4e4c874
Namya LG <namyalg@gmail.com>
2022-06-20 14:57:33 +0530

    include .h docs in .cpp file, write docs for standgrowthcurve

--- Source/moja.modules.cbm/include/moja/modules/cbm/treeyieldtable.h
+++ Source/moja.modules.cbm/include/moja/modules/cbm/treeyieldtable.h
@@ -10,9 +10,6 @@ namespace moja {
 namespace modules {
 namespace cbm {
 
-	/// <summary>
-	/// ADT - Tree yield table
-	/// </summary>
 	class CBM_API TreeYieldTable {
 	public:
 		TreeYieldTable();
@@ -33,9 +30,7 @@ namespace cbm {
 		/// </summary>
 		std::vector<double>& yieldsAtEachAge() { return _yieldsAtEachAge; };
 
-		/*
-		Indexer to get the volume at each age
-		*/
+
 		double operator[](int age) const;
 		
 		/*
@@ -43,38 +38,19 @@ namespace cbm {
 		*/
 		SpeciesType speciesType() const { return _speciesType; }
 
-		/// <summary>
-		/// Create a yield table for a specified type with the transform result yield data
-		/// </summary>
-		/// <param name="yieldTable">It has [Age, Volume]</param>      
-		/// <param name="specieseType">species type, either softwood or hardwood</param>   
+	
 		TreeYieldTable(const std::vector<DynamicObject>& yieldTable,
 					   SpeciesType speciesType);
 	private:
-		/// <summary>
-		/// Load original yield table data[age, volume] 
-		/// Interpolate to get volume at each age
-		/// Calculate the maximum volume
-		/// </summary>
-		/// <param name="rows"></param>
+
 		void Initialize(const std::vector<DynamicObject>& yieldTable);
-
-		/// <summary>
-		/// Lineraly Interpolate the tree yield table to assign the volume at each age year
-		/// </summary>             
+           
 		void InterpolateVolumeAtEachAge();
-
-		/// <summary>
-		/// There should be a volume data at each age. 
-		/// The yield table is not valid if there are ZERO values after the last valid non-zero volume. 
-		/// For example, the volume data like[0,0,0,0,10,20,30,50,60,70,0,0,0,0]. 
-		/// In current CBM, all the trialling ZEROs should be replaced by the last non-zero data - 70.
-		/// The volume data like[0,0,0,0,10,20,30,50,60,70,70,70,70,70].
-		/// </summary>             
+             
 		void preProcessYieldData();
 
 		/// <summary>
-		/// http://stackoverflow.com/questions/12838007/c-sharp-linear-interpolation
+		/// Resource for interpolation : http://stackoverflow.com/questions/12838007/c-sharp-linear-interpolation
 		/// </summary>
 		double linear(double x, double x0, double x1, double y0, double y1);
 		
