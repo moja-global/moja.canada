5417a312050a44bbe58aa98ee846806f78e59938
Namya LG <namyalg@gmail.com>
2022-08-14 14:40:05 +0530

    fix: merge conflict in cbmspinupdisturbancemodule.cpp

--- Source/moja.modules.cbm/src/cbmspinupdisturbancemodule.cpp
+++ Source/moja.modules.cbm/src/cbmspinupdisturbancemodule.cpp
@@ -1,3 +1,12 @@
+/**
+* @file 
+* @brief The CBMSpinupDisturbanceModule module applies the transfers of carbon between 
+* pools defined by the disturbance matrices belonging to the historic and last pass 
+* disturbances in the MAKELIST spin-up procedure.
+* 
+* The MAKELIST spin-up disturbance module does not reset the stand age to 0 if the live 
+* biomass is reduced to zero.
+* ******/
 #include "moja/modules/cbm/cbmspinupdisturbancemodule.h"
 #include "moja/modules/cbm/cbmdisturbanceeventmodule.h"
 #include "moja/modules/cbm/printpools.h"
@@ -14,20 +23,20 @@ namespace moja {
 	namespace modules {
 		namespace cbm {
 
-			/**
-			 * @brief Configuration function
-			 * 
-			 * @param config DynamicObject&
-			 * @return void
-			 * *****************/
+			 /**
+             * @brief Configuration function.
+             * 
+             * @param config DynamicObject&
+             * @return void
+             * ************************/
 			void CBMSpinupDisturbanceModule::configure(const DynamicObject& config) { }
 
-			/**
-			 * @brief Subscribe to signals LocalDomainInit, DisturbanceEvent and TimingInit
-			 * 
+			 /**
+             * @brief Subscribe signals LocalDomainInit,DisturbanceEvent and TimingInit.
+             * 
 			 * @param notificationCenter NotificationCenter&
-			 * @return void
-			 * ************************/
+             * @return void
+             * ************************/
 			void CBMSpinupDisturbanceModule::subscribe(NotificationCenter& notificationCenter) {
 				notificationCenter.subscribe(signals::LocalDomainInit, &CBMSpinupDisturbanceModule::onLocalDomainInit, *this);
 				notificationCenter.subscribe(signals::DisturbanceEvent, &CBMSpinupDisturbanceModule::onDisturbanceEvent, *this);
@@ -35,36 +44,39 @@ namespace moja {
 			}
 
 			/**
-			 * @brief doLocalDomainInit
-			 * 
-			 * Invoke CBMSpinupDisturbanceModule.fetchMatrices(), CBMSpinupDisturbanceModule.fetchDMAssociations(), \n
-			 * assign CBMSpinupDisturbanceModule._spu value of variable "spatial_unit_id" in _landUnitData
-			 * 
-			 * @return void
-			 * ***************************/
+            * @brief Perform at start of simulation.
+			* 
+            * Invoke fetchMatrices() and fetchDMAssociations(). \n
+			* Assign CBMSpinupDisturbanceModule._spu as "spatial_unit_id".
+			* 
+            * @return void
+            * ************************/
+
 			void CBMSpinupDisturbanceModule::doLocalDomainInit() {
 				fetchMatrices();
 				fetchDMAssociations();
 				_spu = _landUnitData->getVariable("spatial_unit_id");
 			}
 
-			/**
-			 * @brief doTimingInit
-			 * 
-			 * Assign CBMSpinupDisturbanceModule._spuId value of CBMSpinupDisturbanceModule._spu
-			 * 
-			 * @return void
-			 * ******************/
+			 /**
+             * @brief Assign CBMSpinupDisturbanceModule._spuId as CBMSpinupDisturbanceModule._spu value.
+             * 
+             * @return void
+             * ************************/
 			void CBMSpinupDisturbanceModule::doTimingInit() {
 				_spuId = _spu->value();
 			}
 
-			/**
-			 * @brief doDisturbanceEvent
+			 /**
+             * @brief doDisturbanceEvent.
+             * 
+			 * Assign boolean variable runPeatland as "peatland_class". \n
+			 * If runPeatland is false, add Carbon budget module disturbance module operation transfer. \n
+			 * else add peatland disturbance module operation transfer.
 			 * 
-			 * @param n DynamicVar 
-			 * @return void
-			 * ***********************/
+             * @param n DynamicVar
+             * @return void
+             * ************************/
 			void CBMSpinupDisturbanceModule::doDisturbanceEvent(DynamicVar n) {
 				auto& data = n.extract<const DynamicObject>();
 
@@ -114,6 +126,19 @@ namespace moja {
 				_landUnitData->submitOperation(disturbanceEvent);
 			}
 
+			 /**
+             * @brief Fetch disturbance matrices.
+             * 
+			 * For each row in "disturbance_matrices", \n
+			 * initialise variable transfer as CBMDistEventTransfer(*_landUnitData, row). \n
+			 * Initialise integer variable dmId as transfer.disturbanceMatrixId(). \n
+			 * if dmId in CBMSpinupDisturbanceModule._matrices is equal to CBMSpinupDisturbanceModule._matrices.end(), \n
+			 * add transfer to eventVector, \n
+			 * dmId and eventVector to CBMSpinupDisturbanceModule._matrices. \n
+			 * else add transfer to v->second.
+			 * 
+             * @return void
+             * ************************/
 			void CBMSpinupDisturbanceModule::fetchMatrices() {
 				_matrices.clear();
 				const auto& transfers = _landUnitData->getVariable("disturbance_matrices")->value()
@@ -135,6 +160,14 @@ namespace moja {
 				}
 			}
 
+			 /**
+             * @brief Insert Disturbance matrix associations.
+             * 
+			 * For each disturbance matrix association in "disturbance_matrix_associations", \n
+			 * insert into CBMSpinupDisturbanceModule._dmAssociations.
+			 * 
+             * @return void
+             * ************************/
 			void CBMSpinupDisturbanceModule::fetchDMAssociations() {
 				_dmAssociations.clear();
 				const auto& dmAssociations = _landUnitData->getVariable("disturbance_matrix_associations")->value()
