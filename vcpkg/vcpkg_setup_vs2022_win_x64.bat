4e721367887d4cec35455f96e321a91366ca780f
mfellows <mfellows@nrcan.gc.ca>
2023-05-23 15:04:45 -0700

    Cleanup.

--- vcpkg/vcpkg_setup_vs2022_win_x64.bat
+++ vcpkg/vcpkg_setup_vs2022_win_x64.bat
@@ -2,21 +2,18 @@
 
 REM Build moja.modules.gcbm dependencies from vcpkg manifest using Visual Studio 2022.
 REM Requires CMake >= 3.26.3.
-set FLINT_VCPKG_DIR="../../../FLINT/vcpkg/vcpkg"
+set FLINT_VCPKG_DIR="%~dp0..\..\FLINT_gdal_upgrade\vcpkg\vcpkg"
 
-pushd ..\source
-
-if not exist build md build
-pushd build
+if not exist ..\source\build md ..\source\build
+pushd ..\source\build
 
 cmake -S .. ^
     -G "Visual Studio 17 2022" ^
+    -DVCPKG_TARGET_TRIPLET=x64-windows ^
+    -DCMAKE_TOOLCHAIN_FILE=%FLINT_VCPKG_DIR%/scripts/buildsystems/vcpkg.cmake ^
+    -DCMAKE_INSTALL_PREFIX=bin ^
     -DPQXX_INCLUDE_DIR=%FLINT_VCPKG_DIR%/packages/libpqxx_x64-windows/include ^
     -DPQXX_LIBRARY=%FLINT_VCPKG_DIR%/packages/libpqxx_x64-windows/bin/pqxx.dll ^
-    -DCMAKE_INSTALL_PREFIX=bin ^
-    -DVCPKG_TARGET_TRIPLET=x64-windows ^
-    -DENABLE_TESTS=OFF ^
-    -DCMAKE_TOOLCHAIN_FILE=%FLINT_VCPKG_DIR%/scripts/buildsystems/vcpkg.cmake
+    -DENABLE_TESTS=OFF
 
-popd
 popd
