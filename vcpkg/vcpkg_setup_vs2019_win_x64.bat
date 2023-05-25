5a1a4f080b581906fd40fcdb8e93b720b79d4158
mfellows <mfellows@nrcan.gc.ca>
2023-05-25 10:55:47 -0700

    Added VS2019 setup script, fixed 2022.

--- vcpkg/vcpkg_setup_vs2019_win_x64.bat
+++ vcpkg/vcpkg_setup_vs2019_win_x64.bat
@@ -1,15 +1,15 @@
 @echo off
 
 REM Build moja.modules.gcbm dependencies from vcpkg manifest using Visual Studio 2022.
-REM Requires CMake >= 3.26.3.
 set FLINT_VCPKG_DIR="%~dp0..\..\FLINT\vcpkg"
 
 if not exist ..\source\build md ..\source\build
 pushd ..\source\build
 
 cmake -S .. ^
-    -G "Visual Studio 17 2022" ^
+    -G "Visual Studio 16 2019" ^
     -DVCPKG_TARGET_TRIPLET=x64-windows ^
+    -DVCPKG_INSTALL_OPTIONS=--x-abi-tools-use-exact-versions ^
     -DCMAKE_TOOLCHAIN_FILE=%FLINT_VCPKG_DIR%/vcpkg/scripts/buildsystems/vcpkg.cmake ^
     -DCMAKE_INSTALL_PREFIX=bin ^
     -DPQXX_INCLUDE_DIR=%FLINT_VCPKG_DIR%/vcpkg/packages/libpqxx_x64-windows/include ^
