94d9bdd52e212a4dc6c2ef6a2c6cfa7614c4f282
Max Fellows <mfellows@nrcan.gc.ca>
2023-05-25 11:22:18 -0700

    Vcpkg manifest (#40)
    * Added vcpkg manifest files - can now built moja.canada and dependencies (VS2022 only for now) using moja.canada\vcpkg\vcpkg_setup_vs2022_win_x64.bat if FLINT was also built the same way.
    
    * Cleanup.
    
    * Added VS2019 setup script, fixed 2022.

---
+++ vcpkg/vcpkg_setup_vs2019_win_x64.bat
@@ -0,0 +1,19 @@
+@echo off
+
+REM Build moja.modules.gcbm dependencies from vcpkg manifest using Visual Studio 2022.
+set FLINT_VCPKG_DIR="%~dp0..\..\FLINT\vcpkg"
+
+if not exist ..\source\build md ..\source\build
+pushd ..\source\build
+
+cmake -S .. ^
+    -G "Visual Studio 16 2019" ^
+    -DVCPKG_TARGET_TRIPLET=x64-windows ^
+    -DVCPKG_INSTALL_OPTIONS=--x-abi-tools-use-exact-versions ^
+    -DCMAKE_TOOLCHAIN_FILE=%FLINT_VCPKG_DIR%/vcpkg/scripts/buildsystems/vcpkg.cmake ^
+    -DCMAKE_INSTALL_PREFIX=bin ^
+    -DPQXX_INCLUDE_DIR=%FLINT_VCPKG_DIR%/vcpkg/packages/libpqxx_x64-windows/include ^
+    -DPQXX_LIBRARY=%FLINT_VCPKG_DIR%/vcpkg/packages/libpqxx_x64-windows/bin/pqxx.dll ^
+    -DENABLE_TESTS=OFF
+
+popd
---
+++ vcpkg/vcpkg_setup_vs2022_win_x64.bat
@@ -0,0 +1,19 @@
+@echo off
+
+REM Build moja.modules.gcbm dependencies from vcpkg manifest using Visual Studio 2022.
+REM Requires CMake >= 3.26.3.
+set FLINT_VCPKG_DIR="%~dp0..\..\FLINT\vcpkg"
+
+if not exist ..\source\build md ..\source\build
+pushd ..\source\build
+
+cmake -S .. ^
+    -G "Visual Studio 17 2022" ^
+    -DVCPKG_TARGET_TRIPLET=x64-windows ^
+    -DCMAKE_TOOLCHAIN_FILE=%FLINT_VCPKG_DIR%/vcpkg/scripts/buildsystems/vcpkg.cmake ^
+    -DCMAKE_INSTALL_PREFIX=bin ^
+    -DPQXX_INCLUDE_DIR=%FLINT_VCPKG_DIR%/vcpkg/packages/libpqxx_x64-windows/include ^
+    -DPQXX_LIBRARY=%FLINT_VCPKG_DIR%/vcpkg/packages/libpqxx_x64-windows/bin/pqxx.dll ^
+    -DENABLE_TESTS=OFF
+
+popd
