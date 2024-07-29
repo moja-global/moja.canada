646f7ffda477f174707de322fe3a4543942decc2
Max Fellows <mfellows@nrcan.gc.ca>
2024-07-29 10:24:25 -0700

    More error message sanitization.

--- Source/moja.modules.cbm/src/flatrecord.cpp
+++ Source/moja.modules.cbm/src/flatrecord.cpp
@@ -295,7 +295,8 @@ namespace cbm {
         if (csvFormat) {
             boost::replace_all(errorStr, "\"", "'");
         } else {
-            boost::replace_all(errorStr, "'", "''");
+            boost::replace_all(errorStr, "&", "\\&");
+            boost::replace_all(errorStr, "'", "\\'");
         }
 
         return (boost::format(csvFormat ? csvRecord : dbRecord)
