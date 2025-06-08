ac5105ce89735d64858091ad825257a698a00b1a
Max Fellows <max.fellows@nrcan.gc.ca>
2025-06-08 11:12:09 -0700

    Added manual commit back in (pqxx does not issue actual commit statement when connecting to ClickHouse)

--- Source/moja.modules.cbm/src/cbmaggregatorhybridlibpqxxwriter.cpp
+++ Source/moja.modules.cbm/src/cbmaggregatorhybridlibpqxxwriter.cpp
@@ -85,6 +85,7 @@ namespace cbm {
                 load(tx, (boost::format("%1%.raw_errors") % _schema).str(), _errorDimension);
                 load(tx, (boost::format("%1%.raw_ages") % _schema).str(), _ageDimension);
                 load(tx, (boost::format("%1%.raw_disturbances") % _schema).str(), _disturbanceDimension);
+                tx.exec("COMMIT");
                 tx.commit();
             });
         }
